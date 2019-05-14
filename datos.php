e705bb129d0c8785c4c8fdb943658c4f0af62c89
Serafin26 <barronlopezlalo-26@outlook.es>
2019-05-14 15:50:45 -0600

    Se crearon los input al formulario

---
+++ datos.php
@@ -0,0 +1,28 @@
+<?php
+
+$nombre =$_GET['nombre'];
+$correo =$_GET['e-mail'];
+$url = $_GET['url'];
+$fecha = $_GET['fecha'];
+$tiempo = $_GET['tiempo'];
+$fechayhora = $_GET['fechayhora'];
+$mes = $_GET['mes'];
+$semana = $_GET['semana'];
+$numero = $_GET['numero'];
+$telefono = $_GET['telefono'];
+$terminobusqueda = $_GET['terminobusqueda'];
+$colorfavorito = $_GET['colorfavorito'];
+
+echo $nombre."<br>";
+echo $correo."<br>";
+echo $url."<br>";
+echo $fecha."<br>";
+echo $tiempo."<br>";
+echo $fechayhora."<br>";
+echo $mes."<br>";
+echo $semana."<br>";
+echo $numero."<br>";
+echo $telefono."<br>";
+echo $terminobusqueda."<br>";
+echo $colorfavorito."<br>";
+?>

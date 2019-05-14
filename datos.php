1859452b842fb437e62fb3696d3bf22e37659528
agustinlcc <50636024+agustinlcc@users.noreply.github.com>
2019-05-14 17:18:01 -0500

    se agrago el css

--- datos.php
+++ datos.php
@@ -1,28 +1,16 @@
 <?php
 
-$nombre =$_GET['nombre'];
-$correo =$_GET['e-mail'];
-$url = $_GET['url'];
-$fecha = $_GET['fecha'];
-$tiempo = $_GET['tiempo'];
-$fechayhora = $_GET['fechayhora'];
-$mes = $_GET['mes'];
-$semana = $_GET['semana'];
-$numero = $_GET['numero'];
-$telefono = $_GET['telefono'];
-$terminobusqueda = $_GET['terminobusqueda'];
-$colorfavorito = $_GET['colorfavorito'];
+$nombre =$_POST['nombre'];
+$correo =$_POST['e-mail'];
+$fecha = $_POST['Fecha'];
+$hora = $_POST['hora'];
+$telefono = $_POST['telefono'];
+$colorfavorito = $_POST['colorfavorito'];
 
 echo $nombre."<br>";
 echo $correo."<br>";
-echo $url."<br>";
 echo $fecha."<br>";
-echo $tiempo."<br>";
-echo $fechayhora."<br>";
-echo $mes."<br>";
-echo $semana."<br>";
-echo $numero."<br>";
+echo $hora."<br>";
 echo $telefono."<br>";
-echo $terminobusqueda."<br>";
 echo $colorfavorito."<br>";
 ?>
